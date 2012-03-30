98a3a2436d3151b3cfd686830a2cd7e6cf7ffb45
Anthony Letourneur <heybob47@hotmail.fr>
2012-03-30 09:40:37 +0200

    Premier commit

---
+++ grails-app/services/reservsalle/ReservationSalleService.groovy
@@ -0,0 +1,24 @@
+package reservsalle
+
+class ReservationSalleService {
+
+    def recherche() {
+
+    }
+	
+	def reserver() {
+		
+	}
+	
+	def annuler(){
+		
+	}
+	
+	def modifier(){
+		
+	}
+	
+	def liste(){
+		
+	}
+}
